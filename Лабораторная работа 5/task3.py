05ba7708f2ce9850b82c285a4fb5cf7c847e826d
VladisloveEgorov <115504383+VladisloveEgorov@users.noreply.github.com>
2022-11-14 20:18:50 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -1,9 +1,8 @@
 import random
 
 
-def get_unique_list_numbers() -> list[int]:
-    n = 16
-    list_ = [random.randint(-10, 10) for _ in range(n)]
+def get_unique_list_numbers(start, stop, num) -> list[int]:
+    list_ = [random.randint(start, stop) for _ in range(num)]
     list_new = []
     for x in list_:
         if x not in list_new:
@@ -11,6 +10,6 @@ def get_unique_list_numbers() -> list[int]:
     return list_new
 
 
-list_unique_numbers = get_unique_list_numbers()
+list_unique_numbers = get_unique_list_numbers(-10, 10, 16)
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
